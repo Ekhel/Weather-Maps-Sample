8aa113c37538ced1aaad8927b0d508e307c6449c
ekhel <blackmanskill@gmail.com>
2020-10-20 22:43:42 +0900

    index

--- src/app.js
+++ src/app.js
@@ -18,11 +18,18 @@ hbs.registerPartials(partialsPath);
 
 app.use(express.static(public));
 
+app.get('', (req, res) => {
+    res.render('weather', {
+        title: 'Weather',
+        name: 'Andrew Mead'
+    })
+})
+
 app.get('/weather', (req, res) => {
-    if(!req.query.address){
-        return res.render('weather', {
-            error: 'Provide search term'
-        });
+    if (!req.query.address) {
+        return res.send({
+            error: 'You must provide an address!'
+        })
     }
 
     geocode(req.query.address, (error, { latitude, longitude, location } = {}) => {
@@ -30,13 +37,12 @@ app.get('/weather', (req, res) => {
             return res.send({ error })
         }
 
-        forecash(latitude, longitude, (error, forecastData) => {
+        forecast(latitude, longitude, (error, forecastData) => {
             if (error) {
                 return res.send({ error })
             }
 
             res.send({
-                title: 'Search Weather',
                 forecast: forecastData,
                 location,
                 address: req.query.address
@@ -59,7 +65,7 @@ app.get('/products', (req, res) => {
     })   
 })
 
-app.get('/help', (req, res) => {
+app.get('/help/*', (req, res) => {
     res.render('help', {
         title: 'Nataours | Help Page',
         name: 'Michael'
