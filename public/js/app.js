0a7a60182389d7749f3caf89d182a85dcb670c64
ekhel <blackmanskill@gmail.com>
2020-10-20 22:52:52 +0900

    fetch change

--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,10 @@
 console.log('cliet Side JS loaded');
 
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+// fetch('http://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
 const searchForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
